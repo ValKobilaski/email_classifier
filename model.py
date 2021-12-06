ea2ea135a5fdc279ae97ecc6a9dbcf41528d5f23
ValKobilaski <kobilaski@gmail.com>
2021-12-06 11:32:13 -0500

    Final version

--- model.py
+++ model.py
@@ -1,63 +1,66 @@
 from sklearn.model_selection import train_test_split
 import numpy as np
-
 import tensorflow as tf
 from tensorflow import keras
-
 from sklearn.metrics import classification_report, confusion_matrix
 
 def main():
+
     x, y = load_data('features.csv')
 
-    x_pca = pca(x)
-    x_pca = np.array(x_pca)
-    x_train, x_test, y_train, y_test = train_test_split(x_pca, y, test_size=0.2, random_state=42)
+    #Normalize data
+    x_n = normalize(x)
+    x_n = np.array(x_n)
+    x_train, x_test, y_train, y_test = train_test_split(x_n, y, test_size=0.2, random_state=0)
 
     model = tf.keras.Sequential()
 
+    #54-12-2-1 FFN
     model.add(tf.keras.layers.Input(shape = (54,)))
-    #100 hidden nodes
-    model.add(tf.keras.layers.Dense(units = 12, activation ='sigmoid'))
-
-    model.add(tf.keras.layers.Dense(units = 2, activation = 'sigmoid'))
-
-    #10 output nodes
+    model.add(tf.keras.layers.Dense(units = 12, activation ='tanh'))
+    model.add(tf.keras.layers.Dense(units = 2, activation = 'tanh'))
     model.add(tf.keras.layers.Dense(units = 1, activation = 'sigmoid'))
 
-    #Using adam over SGD, becasue it gives better results
+    #Implements BP+M and variable learning rate
     model.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics =['accuracy'])
     
-    model.fit(x_train, y_train, epochs = 50)
+    #Train
+    history = model.fit(x_train, y_train, epochs = 50)
 
+    #Evaluate on test data
     prediction = model.predict(x_test)
-
     evaluator(prediction, y_test)
 
 
 
 def load_data(file_name):
+    """
+    Loads data in from file
+    """
     with open(file_name, 'r') as infile:
         data = np.genfromtxt(file_name, delimiter=',')
-        
     x = data[:,:-1]
     y = data[:,-1]
 
     return x,y
 
-def pca(data):
+def normalize(data):
+    """
+    Perform zero-mean standardaization on data
+    """
     x = data
     # calculates the mean
     x -=tf.reduce_mean(data, axis=0)
-    
-    #eigen_values, eigen_vectors = tf.linalg.eigh(tf.tensordot(tf.transpose(x), x, axes=1))
-    #x_pca = tf.tensordot(tf.transpose(eigen_vectors), tf.transpose(x), axes=1)
 
     return x
 
 def evaluator(y_pred, y_test):
+    """
+    Displays confusion matrix and performance metrics for network
+    """
     #confusion martix
     y_pred = np.round(y_pred,0)
-    print(confusion_matrix(y_test, y_pred))
+    print("Confusion Matrix:\n",confusion_matrix(y_test, y_pred))
     #prediction results
     print(classification_report(y_test, y_pred))
 
--- preprocess_emails.py
+++ preprocess_emails.py
@@ -15,23 +15,21 @@ import numpy as np
 def main():
     training_data = os.getcwd()+'/'+'CSDMC2010_SPAM/TRAINING/'
     processed_data_targ = 'CSDMC2010_SPAM/CLEAN' 
-    #clean_dir(training_data, processed_data_targ)
+    clean_dir(training_data, processed_data_targ)
     features = extract_features_dir(processed_data_targ)
     labels = extract_labels('CSDMC2010_SPAM/SPAMTrain.label')
-    print(features.shape)
-    print(str(labels[:,None].shape))
     feature_set = np.concatenate((features.T,labels[:, None].T), axis = 0)
-    print(feature_set.shape)
     np.savetxt("features.csv", feature_set.T, delimiter=",", fmt='%f')
-    #run Once
-    #model = gensim.models.Word2Vec(sentences=training_data, vector_size=100, window=5, min_count=1, workers=4)
-    #model.save("word2vec.model")
-    #model = gensim.models.Word2Vec.load("word2vec.model")
 
 
 def clean_dir(dir_from, dir_to):
+    """
+    Helper function, to take in source directory and target
+    directory and calls clean_file() on each file in the dir
+    """
+
     # Error checking directories
-    print('preprocessing data')
+    print('preprocessing data...')
     if not os.path.exists(dir_from):
         print('The source directory %s does not exist, exit...' % dir_from)
         sys.exit()
@@ -39,96 +37,146 @@ def clean_dir(dir_from, dir_to):
         print('The destination directory is newly created.')
         os.makedirs(dir_to)
 
+    #Loop through files in dir
     files = os.listdir(dir_from)
     for file in files:
         src_file = os.path.join(dir_from, file)
         dest_file = os.path.join(dir_to, file)
 
         src_info = os.stat(src_file)
-        if stat.S_ISDIR(src_info.st_mode):  # for subfolders, recurse
-            #ExtractBodyFromDir(srcpath, dstpath)
+        #For subfolders, recurse
+        if stat.S_ISDIR(src_info.st_mode):
             pass
         else: 
+            #Clean file, and send to destinatoin folder
             output_text = clean_file(dir_from+file)
             with open(dir_to+'/'+file, 'w') as outfile:
                 outfile.write(output_text)
     print('preprocessing complete')
 
+
 def clean_file(file_name):
+    """
+    Takes in a file name, extracts body of email
+    removes html present in .eml format as well as expands
+    contractions for word matching. Retuns body as a str.
+    """
+
+    #Read file
     file = open(file_name, errors = 'ignore')
     body = email.message_from_file(file)
     file.close()
+
+    #Get body of file
     body = body.get_payload()
     if type(body) == type(list()):
         body = body[0]
     if type(body) != type(''):
         body = str(body)
+
+    #Clean file
     body = _remove_html_tags(body)
     body = _expand_contractions(body)
     return body
 
+
 def extract_features_dir(src_dir):
+    """
+    Helper function, to extract features of every file
+    within a source directory, returns numpy array of features
+    """
+
+    #Count files
     datapoints = len(os.listdir(src_dir))
     features = np.zeros((datapoints, 54))
     p = 0
 
-    print('extracting features...')
+    #Error checkign directory
+    print('Extracting features...')
     if not os.path.exists(src_dir):
         print('The source directory %s does not exist, exit...' % src_dir)
         sys.exit()
 
+    #Extract features of each file
     files = os.listdir(src_dir)
     for file in files:
         features[p] = extract_features_file(src_dir + '/' + file)
         p+= 1
-
+    print('Feature Extraction complete')
     return features
 
     
-
 def extract_features_file(file_name):
+    """
+    Takes in input file, reads file, extracts features
+    and does further cleaning, returns numpy array
+    of feature vector for specified file
+    """
+
+    #Read filter word file
     with open('filter_words.txt', 'r') as word_file:
         freq_words = word_file.read()
         freq_words = freq_words.split('\n')
 
+    #Read input file
     with open(file_name, 'r') as infile:
         text = infile.read()
     text.replace("\n", " ")
 
-    char_data = _get_char_data(text)    # returns 0-2
+    #Feature extraction and further cleaning
+    char_data = _get_char_data(text)    #Returns features 0-2
     text = _remove_punctuation(text)
-    capital_data = _get_capital_runs(text) # returns 3-5
+    capital_data = _get_capital_runs(text) #Returns features 3-5
     text = text.lower()
-    word_data = _get_word_freq(freq_words,text)    # returns 6-54
+    word_data = _get_word_freq(freq_words,text)    #Returns features 6-54
 
+    #combine features in to 1 vector
     data_vector = char_data + capital_data + word_data
     
+    #Return Vector
     return np.array(data_vector)
 
+
+
+
+
 def extract_labels(file_name):
+    """
+    Extracts output labels for specified file
+    in our case "SPAMTrain.label"
+    """
     y_labels = []
     # files = []
 
+    #Read file line by line
     with open(file_name, 'r', encoding='utf-8') as infile:
         for line in infile:
             line = line.split()
             y_labels.append(line[0])
+
+    #Return numpy vector of labels
     return np.array(y_labels, dtype = float)
 
 
 def _remove_html_tags(text):
-    """Remove html tags from a string"""
+    """
+    Remove html tags from a string
+    """
     clean = re.compile('<.*?>')
     return re.sub(clean, ' ', text)
-            
+
+
 def _remove_punctuation(text):
     return text.translate(str.maketrans('', '', string.punctuation))
+
 
 def _expand_contractions(text):
     return contractions.fix(text)
 
+
 def _tokenize_string(text):
     return word_tokenize(text)
+
 
 def _remove_stopwords(text_arr):
     filtered = []
@@ -137,13 +185,19 @@ def _remove_stopwords(text_arr):
             filtered.append(word)
     return filtered
 
+
 def _stem_text(text_arr):
     stem_arr = []
     for word in text_arr:
         stem_arr.append(PS.stem(word))
     return stem_arr
 
+
 def _get_char_data(text):
+    """
+    Reads string, returns chracter feature data,
+    as standard python array
+    """
     chars = {'!' : 0, '#' : 0, '$' : 0}
     for char in text:
         if char == '!':
@@ -154,7 +208,12 @@ def _get_char_data(text):
             chars['$'] += 1
     return[chars['!'], chars['#'], chars['$']]
 
+
 def _get_capital_runs(text):
+    """
+    Reads text, counts the number of capitals
+    and the longest run of capitals >= 2.
+    """
     capitals = []
     
     curr_count = 0
@@ -165,9 +224,11 @@ def _get_capital_runs(text):
             curr_count +=1
         else:
             if (curr_count >= 2):
+                #Keep track of run
                 capitals.append(curr_count)
             curr_count = 0
     if(len(capitals) != 0 ):
+        #In case no capitals avoid division by 0
         average = sum(capitals) / len(capitals)
         longest = max(capitals)
     else:
@@ -180,18 +241,25 @@ def _get_capital_runs(text):
 
 
 def _get_word_freq(words, text):
+    """
+    Creates python dictionary from filter words
+    reads file, and counts occurances of words
+    """
+    #Create dictionary
     count = {w : 0 for w in words}
-    text_list = text.split(" ")
 
-    
+    #Reads text and checks for filter words
+    text_list = text.split(" ")
     for freq_word in count.keys():
         for word in text_list:
             if freq_word in word:
                 count[freq_word] += 1
     
+    #Creates data feature
     for freq_word in count.keys():
         count[freq_word] = count[freq_word] / len(text_list)
 
+    #Returns python list for word frequencies
     return list(count.values())
 
 
